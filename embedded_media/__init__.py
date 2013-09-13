7807ef2e83026272b971808d8ee546a2c2787971
Jason Kraus <zbyte64@gmail.com>
2013-09-13 12:30:00 -0700

    fix for pip installing

--- embedded_media/__init__.py
+++ embedded_media/__init__.py
@@ -4,4 +4,11 @@ __description__ = 'Directly embed CSS and JS in Django forms media.'
 __version__ = '0.0.1'
 __license__ = 'MIT License'
 
-from .media import CSS, JS
+try:
+    import django
+except ImportError:
+    pass
+    #we are pip installing
+else:
+    from .media import CSS, JS
+
